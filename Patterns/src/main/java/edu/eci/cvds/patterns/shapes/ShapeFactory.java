ffeb4b76bb6966514cc14f42376ddf3f30e85e95
FelipeRojas15 <53972640+FelipeRojas15@users.noreply.github.com>
2019-08-17 10:04:52 -0500

    Agrego Trabajo en clase Lab2

---
+++ Patterns/src/main/java/edu/eci/cvds/patterns/shapes/ShapeFactory.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cvds.patterns.shapes;
+
+/**
+ *
+ * @author 2153043
+ */
+public class ShapeFactory {
+     
+    public static Shape creat(RegularShapeType type) {
+        Shape shape;
+        switch(type){
+            case Triangle:
+                shape = new Triangle;
+                
+        }
+                
+    }
+    
+}
