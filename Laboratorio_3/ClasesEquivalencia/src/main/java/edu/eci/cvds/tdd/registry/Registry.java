e794810e50533f930d539cd2be5686f803114be1
unknown <2153043@labinfo.is.escuelaing.edu.co>
2019-08-29 15:45:16 -0500

    Avance lab 3

--- Laboratorio_3/ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ Laboratorio_3/ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -22,23 +22,14 @@ public class Registry {
         	return RegisterResult.UNDERAGE;
         }
         
-        //esto se suponia que miraba si un man ya estaba registrado no?
-        /**
-    	else if (p.getAge() >= 18 && p.isAlive()) {           antes esta asi
-        	return RegisterResult.DUPLICATED;   
-        }
-        */ 
         
-        else if (identificador.contains(p.getId())){        //ahora asi, no se si la cague pero lo de arriba se veia muy extraño
+        else if (identificador.contains(p.getId())){       
             return RegisterResult.DUPLICATED;
         }
         
-        //}else if(p.getAge() >= 18 && p.isAlive()){
-          //      return RegisterResult.VALID;              //esta vaina sobraba porque al final ya todo eso se reviso
-        //} 
+     
         
-        
-        identificador.add(p.getId());       //al final agrega el id del man a la lista y bota valid
+        identificador.add(p.getId());      
         return RegisterResult.VALID;
         
     }
