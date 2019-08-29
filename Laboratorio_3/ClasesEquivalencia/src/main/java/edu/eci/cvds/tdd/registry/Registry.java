1a5f6b58aca32bcdf89cefbf06895be5147e7e83
Kyrie <james.weisnner@mail.escuelaing.edu.co>
2019-08-29 13:29:32 -0500

    corregido Registry, RegistryTest terminado, CalculadorDescuentos terminado, ExcepcionParametrosInvalidos creada

--- Laboratorio_3/ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ Laboratorio_3/ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -3,7 +3,9 @@ package edu.eci.cvds.tdd.registry;
 import java.util.*;
 
 public class Registry {
-    
+    /**
+       @param identificador lista de identificaciones de votanes 
+    */
     public ArrayList<Integer> identificador;
     public Registry() {
 		identificador = new ArrayList<>();
@@ -16,14 +18,28 @@ public class Registry {
     	else if (p.getAge()<0) {
     		return RegisterResult.INVALID_AGE; 
     	}
-    	else if (p.getAge()<18 && p.isAlive() == true) {
+    	else if (p.getAge()<18 && p.isAlive()) {
         	return RegisterResult.UNDERAGE;
         }
-    	else if (p.getAge() >= 18 && p.isAlive()==true ) {
-        	return RegisterResult.DUPLICATED; 
-        }else if(p.getAge() >= 18 && p.isAlive() == true){
-                return RegisterResult.VALID;
-        } 
+        
+        //esto se suponia que miraba si un man ya estaba registrado no?
+        /**
+    	else if (p.getAge() >= 18 && p.isAlive()) {           antes esta asi
+        	return RegisterResult.DUPLICATED;   
+        }
+        */ 
+        
+        else if (identificador.contains(p.getId())){        //ahora asi, no se si la cague pero lo de arriba se veia muy extraño
+            return RegisterResult.DUPLICATED;
+        }
+        
+        //}else if(p.getAge() >= 18 && p.isAlive()){
+          //      return RegisterResult.VALID;              //esta vaina sobraba porque al final ya todo eso se reviso
+        //} 
+        
+        
+        identificador.add(p.getId());       //al final agrega el id del man a la lista y bota valid
         return RegisterResult.VALID;
+        
     }
 }
--- Laboratorio_3/ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ Laboratorio_3/ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -12,10 +12,36 @@ public class RegistryTest {
     	registry = new Registry();
     	Person personaA= new Person("Marcelo Vaca",01,8,Gender.MALE,true);
     	 RegisterResult result = registry.registerVoter(personaA);
-         Assert.assertEquals(RegisterResult.UNDERAGE,result);
-
-        
+         Assert.assertEquals(RegisterResult.UNDERAGE,result);        
+    }    
+    @Test    
+    public void debeEstarMuerto(){
+        registry = new Registry();
+        Person personaB = new Person("Jesse Ventura",02,38,Gender.MALE,false);
+        RegisterResult result = registry.registerVoter(personaB);
+        Assert.assertEquals(RegisterResult.DEAD,result);
     }
-    
+    @Test
+    public void debeTenerEdadInvalida(){
+        registry = new Registry();
+        Person personaC = new Person("El atrocidad",03,-19,Gender.UNIDENTIFIED,true);
+        RegisterResult result = registry.registerVoter(personaC);
+        Assert.assertEquals(RegisterResult.INVALID_AGE,result);
+    }
+    @Test
+    public void debeEstarDuplicado(){
+        registry = new Registry();
+        Person personaD = new Person("Simon Marin",04, 19,Gender.MALE,true);
+        registry.registerVoter(personaD);
+        RegisterResult result = registry.registerVoter(personaD);
+        Assert.assertEquals(RegisterResult.DUPLICATED,result);
+    }
+    @Test
+    public void debeSerValido (){
+        registry = new Registry();
+        Person personaE = new Person("James Weisnner",52,21,Gender.MALE,true);
+        RegisterResult result = registry.registerVoter(personaE);
+        Assert.assertEquals(RegisterResult.VALID,result);
+    }
     
 }
