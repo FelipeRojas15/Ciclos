eff4e6227cdd670c318dc1b667e13d12c2758b2d
FelipeRojas15 <53972640+FelipeRojas15@users.noreply.github.com>
2019-08-30 22:28:57 -0500

    Pruebas Punto 1

--- Laboratorio_3/ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ Laboratorio_3/ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -5,21 +5,39 @@ import org.junit.Test;
 
 public class RegistryTest {
 
-    //private Registry registry = new Registry();
-    /**
+    private Registry registry = new Registry();
+    
     @Test
     public void debeSerMenorDeEdad() {
     	registry = new Registry();
-    	Person personaA= new Person("Marcelo Vaca",01,8,Gender.MALE,true);
-    	 RegisterResult result = registry.registerVoter(personaA);
-         Assert.assertEquals(RegisterResult.UNDERAGE,result);        
+    	Person personaA= new Person("Marcelo Vaca",1,8,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(personaA);
+        Assert.assertEquals(RegisterResult.UNDERAGE,result);     
+        Person personB = new Person("Radamel Garcia",2,1,Gender.MALE,true);
+        result = registry.registerVoter(personB);
+        Assert.assertEquals(RegisterResult.UNDERAGE,result);
+        Person personC = new Person("Danny Marin", 4,-1, Gender.MALE, true); //limite superior
+        result = registry.registerVoter(personC);
+        Assert.assertEquals(RegisterResult.INVALID_AGE,result);
+        Person personaD= new Person("Andres Gomez",1,17,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(personaD);
+        Assert.assertEquals(RegisterResult.UNDERAGE,result); 
+        Person personaE= new Person("Paulo Londra",1,19,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(personaE);
+        Assert.assertEquals(RegisterResult.VALID,result); 
     }    
     @Test    
     public void debeEstarMuerto(){
         registry = new Registry();
-        Person personaB = new Person("Jesse Ventura",02,38,Gender.MALE,false);
+        Person personaB = new Person("Jesse Ventura",4,38,Gender.MALE,false);
         RegisterResult result = registry.registerVoter(personaB);
         Assert.assertEquals(RegisterResult.DEAD,result);
+        Person personC = new Person("Felipe Bernal", 3,288, Gender.MALE, false); 
+        result = registry.registerVoter(personC);
+        Assert.assertEquals(RegisterResult.DEAD,result);
+        Person personaE= new Person("Paulo Londra",1,19,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(personaE);
+        Assert.assertEquals(RegisterResult.VALID,result);
     }
     @Test
     public void debeTenerEdadInvalida(){
@@ -27,6 +45,12 @@ public class RegistryTest {
         Person personaC = new Person("El atrocidad",03,-19,Gender.UNIDENTIFIED,true);
         RegisterResult result = registry.registerVoter(personaC);
         Assert.assertEquals(RegisterResult.INVALID_AGE,result);
+        Person personaE= new Person("Paulo Londra",1,-1,Gender.MALE,true);
+    	RegisterResult result = registry.registerVoter(personaE);
+        Assert.assertEquals(RegisterResult.INVALID_AGE,result);
+        Person personF = new Person("Danny Marin", 4,0, Gender.MALE, true); //limite superior
+        result = registry.registerVoter(personF);
+        Assert.assertEquals(RegisterResult.VALID,result);
     }
     @Test
     public void debeEstarDuplicado(){
@@ -35,6 +59,12 @@ public class RegistryTest {
         registry.registerVoter(personaD);
         RegisterResult result = registry.registerVoter(personaD);
         Assert.assertEquals(RegisterResult.DUPLICATED,result);
+        Person personC = new Person("Danny Marin", 5,0, Gender.MALE, true); //limite superior
+        result = registry.registerVoter(personC);
+        Assert.assertEquals(RegisterResult.VALID,result);
+        Person personE = new Person("Danny Marin", 5,19, Gender.MALE, true);
+        result = registry.registerVoter(personE);
+        Assert.assertEquals(RegisterResult.DUPLICATED,result);
     }
     @Test
     public void debeSerValido (){
@@ -42,6 +72,12 @@ public class RegistryTest {
         Person personaE = new Person("James Weisnner",52,21,Gender.MALE,true);
         RegisterResult result = registry.registerVoter(personaE);
         Assert.assertEquals(RegisterResult.VALID,result);
+        Person personA = new Person("CVDS", 1,35, Gender.MALE, true);
+       RegisterResult result = registry.registerVoter(personA);
+       Assert.assertEquals(RegisterResult.VALID,result);   
+       Person personB = new Person("Juliana Garcia", 2,20, Gender.FEMALE, true);
+       result = registry.registerVoter(personB);
+       Assert.assertEquals(RegisterResult.VALID,result);
     }
-   */ 
+    
 }
